29d6776273c13042dac92442e9591523da1ec2c5
Elvis <elvismrema9@gmail.com>
2025-02-06 16:56:17 +0300

    🥋 echo now

---
+++ resources/views/admin/category.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    @include('admin.css')
+    <style type="text/css">
+        .div_center{
+            text-align: center;
+            padding: 40px;
+        }
+        .h2_font {
+            font-size: 40px;
+            padding-bottom: 40px;
+        }
+        .input_color{
+            color: black;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-scroller">
+        <!-- partial:partials/_sidebar.html -->
+        @include('admin.sidebar')
+        <!-- partial -->
+        @include('admin.header')
+        <!-- partial -->
+        <div class="main-panel">
+            <div class="content-wrapper">
+                @if(session()->has('message'))
+                    <div class="alert alert-success">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                        {{session()->get('message')}}
+                    </div>
+                @endif
+                <div class="div_center">
+                    <h1 class="h2_font">Add category</h1>
+                    <form action="{{url('add_category')}}" method="POST">
+                        @csrf
+                        <input class="input_color" type="text" name="category" placeholder="Write category name">
+                        <input type="submit" class="btn btn-primary" name="submit" value="Add category">
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- container-scroller -->
+        <!-- plugins:js -->
+        @include('admin.script')
+        <!-- End custom js for this page -->
+</body>
+
+</html>
