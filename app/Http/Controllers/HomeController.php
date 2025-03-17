16859ea9118a0612f8c732a3af41c760d61b5927
Elvis <elvismrema9@gmail.com>
2025-03-17 09:32:29 +0300

    🧘‍ some minor changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
@@ -14,11 +15,13 @@ class HomeController extends Controller
         if($usertype == '1'){
             return view('admin.home');
         }else{
-            return view('home.userpage');
+            $product = product::paginate(10);
+            return view('home.userpage', compact('product'));
         }
     }
 
     public function index(){
-        return view('home.userpage');
+        $product = product::paginate(10);
+        return view('home.userpage', compact('product'));
     }
 }
