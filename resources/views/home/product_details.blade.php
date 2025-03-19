64465cca7fc42d2948ed693c3709f13f73c44029
Elvis <elvismrema9@gmail.com>
2025-03-19 12:45:51 +0300

    add and remove product to cart ✅

--- resources/views/home/product_details.blade.php
+++ resources/views/home/product_details.blade.php
@@ -62,7 +62,17 @@
                         <h6>Product description : {{$product->description}}</h6>
                         <h6>Available quantity : {{$product->quantity}}</h6>
 
-                        <a href="" class="btn btn-primary">Add to cart</a>
+                        <form action="{{url('add_cart', $product->id)}}" method="POST">
+                           @csrf
+                           <div class="row">
+                              <div class="col-md-4">
+                                 <input type="number" name="quantity" value="1" min="" style="width: 100px;">
+                              </div>
+                              <div class="col-md-4">
+                                 <input type="submit" value="Add To Cart">
+                              </div>
+                           </div>
+                           </form>
                      </div>
                   </div>
                </div>
